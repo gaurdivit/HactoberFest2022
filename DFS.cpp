5842d3b99eff9c4fcd9d378af04178ed4142e325
Divit Gaur <93473858+gaurdivit@users.noreply.github.com>
2022-10-15 05:38:22 +0530

    nQueens Question

--- DFS.cpp
+++ DFS.cpp
@@ -1,45 +1,67 @@
-#include<bits/stdc++.h>
+// C++ program to print DFS traversal from
+// a given vertex in a given graph
+#include <bits/stdc++.h>
 using namespace std;
-void print(int **edges,int n,int si,bool *visited){
-    cout<<si<<" ";
-    visited[si]=true;
-    for(int i=0;i<n;i++){
-        if(i==si){
-            continue;
-        }
-        if(edges[si][i]==1){
-            if(visited[i]){
-                continue;
-            }
-            print(edges,n,i,visited);
-        }
-    }
+
+// Graph class represents a directed graph
+// using adjacency list representation
+class Graph {
+public:
+	map<int, bool> visited;
+	map<int, list<int> > adj;
+
+	// function to add an edge to graph
+	void addEdge(int v, int w);
+
+	// DFS traversal of the vertices
+	// reachable from v
+	void DFS(int v);
+};
+
+void Graph::addEdge(int v, int w)
+{
+	adj[v].push_back(w); // Add w to v’s list.
 }
-int main() {
-  int n,e;
-  cin>>n>>e;
-  int **edges=new int*[n];
-  for(int i=0;i<n;i++){
-    edges[i]=new int[n];
-    for(int j=0;j<n;j++){
-        edges[i][j]=0;
-    }
-  }
-    for(int i=0;i<e;i++){
-        int f,s;
-        cin>>f>>s;
-        edges[f][s]=1;
-        edges[s][f]=1;
-    }
-    bool *visited=new bool[n];
-    for(int i=0;i<n;i++){
-        visited[i]=false;
-    }
-    print(edges,n,0,visited);
-    for(int i=0;i<n;i++){
-        delete []edges[i];
-    }
-    delete []edges;
-    delete [] visited;
-    return 0;
+
+void Graph::DFS(int v)
+{
+	// Mark the current node as visited and
+	// print it
+	visited[v] = true;
+	cout << v << " ";
+
+	// Recur for all the vertices adjacent
+	// to this vertex
+	list<int>::iterator i;
+	for (i = adj[v].begin(); i != adj[v].end(); ++i)
+		if (!visited[*i])
+			DFS(*i);
 }
+
+// Driver's code
+int main()
+{
+	// Create a graph given in the above diagram
+	Graph g;
+	g.addEdge(0, 1);
+	g.addEdge(0, 2);
+	g.addEdge(1, 2);
+	g.addEdge(2, 0);
+	g.addEdge(2, 3);
+	g.addEdge(3, 3);
+
+	cout << "Following is Depth First Traversal"
+			" (starting from vertex 2) \n";
+
+	// Function call
+	g.DFS(2);
+
+	return 0;
+}
+
+// improved by Vishnudev C
+
+
+// name : Vinod Kuril
+// aim : To contribute
+// username : Vinodk-17
